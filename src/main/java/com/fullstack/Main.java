c39b9159a33382c63f52e89efdcf0df815cb2d82
Amny-coder <amny.inc@gmail.com>
2022-12-28 06:39:36 +0100

    main class

--- src/main/java/com/fullstack/Main.java
+++ src/main/java/com/fullstack/Main.java
@@ -1,11 +1,18 @@
 package com.fullstack;
 
 import javax.naming.NamingException;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws NamingException {
-        
-        TableUpdate update = new TableUpdate();
-        update.updateTableDB();
+    public static void main(String[] args) throws NamingException, SQLException {
+
+        BatchUpdate batch = new BatchUpdate();
+        batch.batch();
+
+        ConcurrentUpdate up = new ConcurrentUpdate();
+        up.updatePrices();
+
+        MyJDBC jdbc = new MyJDBC();
+        jdbc.connector();
     }
 }
