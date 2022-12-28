ac6c1cee8bd7615a99cb9e3a621dd7c2896f1486
Amny-coder <amny.inc@gmail.com>
2022-12-28 06:41:07 +0100

    reading car price from database

--- src/main/java/com/fullstack/MyJDBC.java
+++ src/main/java/com/fullstack/MyJDBC.java
@@ -1,8 +1,6 @@
 package com.fullstack;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MyJDBC {
     public final String url = "jdbc:postgresql://localhost:5432/test";
@@ -11,8 +9,15 @@ public class MyJDBC {
 
     public void connector() {
         try(Connection conn = DriverManager.getConnection(url, user, password);) {
-            if (conn != null)
+            if (conn != null) {
                 System.out.println("Connected!");
+                Statement stm = conn.createStatement();
+                ResultSet rs = stm.executeQuery("SELECT * FROM car");
+                while (rs.next()) {
+                    double price = rs.getDouble("PRICE");
+                    System.out.println("Final price: " + price);
+                }
+            }
             else
                 System.out.println("Not connected!");
             
