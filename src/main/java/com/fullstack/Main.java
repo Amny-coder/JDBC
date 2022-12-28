02f36ceb0d5f98298c80eb0ee36a3124063f4a54
Amny-coder <amny.inc@gmail.com>
2022-12-28 10:34:09 +0100

    main class

--- src/main/java/com/fullstack/Main.java
+++ src/main/java/com/fullstack/Main.java
@@ -6,13 +6,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws NamingException, SQLException {
 
-        BatchUpdate batch = new BatchUpdate();
-        batch.batch();
-
-        ConcurrentUpdate up = new ConcurrentUpdate();
-        up.updatePrices();
-
-        MyJDBC jdbc = new MyJDBC();
-        jdbc.connector();
+        CoffeeSales coffee = new CoffeeSales();
+        coffee.coffeesTable();
     }
 }
