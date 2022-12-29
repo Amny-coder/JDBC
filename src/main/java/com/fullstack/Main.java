01e6147cc25bb858d9fc1df2bb1cd4ed678a8e4b
Amny-coder <amny.inc@gmail.com>
2022-12-29 10:41:27 +0100

    Main Class

--- src/main/java/com/fullstack/Main.java
+++ src/main/java/com/fullstack/Main.java
@@ -2,11 +2,17 @@ package com.fullstack;
 
 import javax.naming.NamingException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws NamingException, SQLException {
 
-        CoffeeSales coffee = new CoffeeSales();
-        coffee.coffeesTable();
+        Transaction transaction = new Transaction();
+        HashMap<String, Integer> db = new HashMap<>();
+
+        db.put("German-coff", 15);
+        db.put("Local-coff", 10);
+        transaction.updateCoffeeSales(db);
     }
 }
