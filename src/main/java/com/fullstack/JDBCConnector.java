ff31eb4aedeeb4e4db93592431ac472a21de5b0c
Usman Muhammad <110188033+Amny-coder@users.noreply.github.com>
2023-04-04 10:10:01 +0100

    Code formatted for easy readability

--- src/main/java/com/fullstack/JDBCConnector.java
+++ src/main/java/com/fullstack/JDBCConnector.java
@@ -5,12 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCConnector {
+    
     private Connection con = null;
+    
     public void connector(String url) {
+        
         if (url != null) {
             try {
+                
                 con = DriverManager.getConnection(url);
+                
             } catch (SQLException e) {
+                
                 System.out.println("Failed to connect --> ");
                 e.printStackTrace();
             }
